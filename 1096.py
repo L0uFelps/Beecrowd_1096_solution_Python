e588c799da4a7e42c1f18604cdaa83d7014668d1
L0ufelps <felipeluis2803@gmail.com>
2023-02-23 21:51:09 -0300

    Exercicio Resolvido

---
+++ 1096.py
@@ -0,0 +1,14 @@
+i = 1
+j = 7
+
+contador = 1
+while contador < 15:
+    print("I={} J=7{}".format(i,j))
+    j = j - 1
+    print("I={} J=7{}".format(i,j))
+    j = j - 1
+    print("I={} J=7{}".format(i,j))
+    i = 3
+    j = 7
+    print("I={} J=7{}".format(i,j))
+    
